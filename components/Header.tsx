b4a9ade6191db93a9eafc9e7493e63d7efc33cba
Sam Makhlouf <maksam7dev@gmail.com>
2022-09-30 16:07:38 +0100

    init

---
+++ components/Header.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { SocialIcon } from "react-social-icons"
+
+type Props = {}
+
+export default function Header({}: Props) {
+   return (
+      <header>
+         <div className="flex flex-row items-center">
+            {/* Social Icons */}
+            <SocialIcon url="https://facebook.com/" fgColor="gray" bgColor="transparent" />
+            <SocialIcon url="https://twitter.com/" fgColor="gray" bgColor="transparent" />
+            <SocialIcon url="https://linkedin.com/" fgColor="gray" bgColor="transparent" />
+            <SocialIcon url="https://youtube.com/" fgColor="gray" bgColor="transparent" />
+         </div>
+
+         <div>
+            <SocialIcon className="cursor-pointer" network="email" fgColor="gray" bgColor="transparent" />
+         </div>
+      </header>
+   )
+}
